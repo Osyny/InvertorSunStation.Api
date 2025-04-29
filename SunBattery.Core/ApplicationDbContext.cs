a8911daa4861601050b7e5eff95d379933aa13aa
Osyny <oxana1404@gmail.com>
2025-04-29 15:58:19 +0300

    added migration SeedRoles

--- SunBattery.Core/ApplicationDbContext.cs
+++ SunBattery.Core/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SunBattery.Core.Entities;
 
@@ -15,11 +16,22 @@ namespace SunBattery_Api
 
         public DbSet<ProtocolData> ProtocolDatas { get; set; }
         public DbSet<InfoTime> InfoTimes { get; set; }
-    }
-    //public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-    //{
 
-    //    public DbSet<ProtocolData> ProtocolDatas { get; set; }
-    //    public DbSet<InfoTime> InfoTimes { get; set; }
-    //}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        }
+
+        private static void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData
+                (
+                    new IdentityRole() { Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+                    new IdentityRole() { Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" }
+                );
+        }
+
+    }
 }
